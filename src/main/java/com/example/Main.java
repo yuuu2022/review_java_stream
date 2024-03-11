cf91219510cb0f10b77e5643e5a13b67037f300e
Kim <pang921008@gmail.com>
2024-03-11 13:44:22 +0800

    init java project for coding

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static List<String> list = new ArrayList<String>();
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(list.size());
+
+
+
+    }
+}
